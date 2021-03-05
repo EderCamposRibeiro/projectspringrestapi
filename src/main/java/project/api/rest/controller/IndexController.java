2a4496ba9f9142933ce27b2b0d2a2ce2b49d1133
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-03-05 18:16:23 -0300

    Project Created

---
+++ src/main/java/project/api/rest/controller/IndexController.java
@@ -0,0 +1,17 @@
+package project.api.rest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class IndexController {
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity init() {
+		return new ResponseEntity("Hello User REST Spring Boot", HttpStatus.OK);
+	}
+}
