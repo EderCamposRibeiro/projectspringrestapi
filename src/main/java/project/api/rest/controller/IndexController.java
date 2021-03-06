b12b8c2df01562ede81e87993228bc49018e4c25
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-03-06 08:44:47 -0300

    Returning the JSON user to the screen

--- src/main/java/project/api/rest/controller/IndexController.java
+++ src/main/java/project/api/rest/controller/IndexController.java
@@ -1,11 +1,15 @@
 package project.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import project.api.rest.model.System_User;
 
 @RestController/*REST Architecture*/
 @RequestMapping(value = "/user")
@@ -13,9 +17,24 @@ public class IndexController {
 	
 	/*RESTful service*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init(@RequestParam (value = "name", required = true, defaultValue = "The name is required!") String name, 
-			                   @RequestParam("income")  Long income) {
-		System.out.println("Received parameter " + name + " / " + income);
-		return new ResponseEntity("Hello User REST Spring Boot, your name is: " + name + " Income: R$: " + income, HttpStatus.OK);
+	public ResponseEntity<System_User> init() {
+		
+		System_User sysuser = new System_User();
+		sysuser.setId(50L);
+		sysuser.setLogin("edercribeiro@gmail.com");
+		sysuser.setName("Eder Campos Ribeiro Mazzoccante");
+		sysuser.setPassword("ABcd1234");
+		
+		System_User sysuser2 = new System_User();
+		sysuser2.setId(51L);
+		sysuser2.setLogin("ederteste@gmail.com");
+		sysuser2.setName("Eder Campos Ribeiro");
+		sysuser2.setPassword("00223200");		
+		
+		List<System_User> users = new ArrayList<System_User>();
+		users.add(sysuser);
+		users.add(sysuser2);
+		
+		return new ResponseEntity(users, HttpStatus.OK);
 	}
 }
