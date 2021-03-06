2880414fe658f928fad528c74d88f96e312cbf7e
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-03-06 17:04:49 -0300

    Creating our POST service and making a user registration

--- src/main/java/project/api/rest/controller/IndexController.java
+++ src/main/java/project/api/rest/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,7 @@ public class IndexController {
 	private UserRepository userRepository;
 	
 	/*RESTful service*/
-	@GetMapping(value = "/{id}/salecode/{sale}", produces = "application/pdf")
+	@GetMapping(value = "/{id}/salecode/{sale}", produces = "application/json")
 	public ResponseEntity<System_User> report(@PathVariable(value = "id") Long id
 			                                , @PathVariable(value = "sale") Long sale) {
 		
@@ -48,7 +50,34 @@ public class IndexController {
 		
 		return new ResponseEntity<List<System_User>>(list, HttpStatus.OK);
 	}
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<System_User> register(@RequestBody System_User system_user){
+		
+		System_User saveduser = userRepository.save(system_user);
+		
+		return new ResponseEntity<System_User>(saveduser, HttpStatus.OK);
+		
+	}
+	
+	@PostMapping(value = "/{iduser}/idsell/{idsell}", produces = "application/json")
+	public ResponseEntity<System_User> registerSell(@PathVariable Long iduser,
+			                                        @PathVariable Long idsell){
+		
+		/*Here would be the sell process*/
+		//System_User saveduser = userRepository.save(system_user);
+		
+		return new ResponseEntity("Id user: " + iduser + " Id sell: " + idsell, HttpStatus.OK);
+		
+	}
+	
 }
+
+
+
+
+
+
 
 
 
