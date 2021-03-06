cd4030e0dd3c5c29117c358086b3d5d4a38fae38
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-03-06 08:13:02 -0300

    Passing parameters to the restful service

--- src/main/java/project/api/rest/controller/IndexController.java
+++ src/main/java/project/api/rest/controller/IndexController.java
@@ -4,14 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController/*REST Architecture*/
 @RequestMapping(value = "/user")
 public class IndexController {
 	
+	/*RESTful service*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
-		return new ResponseEntity("Hello User REST Spring Boot", HttpStatus.OK);
+	public ResponseEntity init(@RequestParam (value = "name", required = true, defaultValue = "The name is required!") String name, 
+			                   @RequestParam("income")  Long income) {
+		System.out.println("Received parameter " + name + " / " + income);
+		return new ResponseEntity("Hello User REST Spring Boot, your name is: " + name + " Income: R$: " + income, HttpStatus.OK);
 	}
 }
