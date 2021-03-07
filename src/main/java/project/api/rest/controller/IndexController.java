3d659d2629cfc5ff22e5cc6c616655931eeb0e9c
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-03-07 09:25:56 -0300

    Creating our PUT/update and Delete

--- src/main/java/project/api/rest/controller/IndexController.java
+++ src/main/java/project/api/rest/controller/IndexController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +45,14 @@ public class IndexController {
 		return new ResponseEntity<System_User>(user.get(), HttpStatus.OK);
 	}
 	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete(@PathVariable("id") Long id) {
+		
+		userRepository.deleteById(id);
+		
+		return "OK";
+	}
+	
 	@GetMapping(value = "/", produces = "application/json")
 	public ResponseEntity<List<System_User>> users (){
 		
@@ -63,13 +73,24 @@ public class IndexController {
 	@PostMapping(value = "/{iduser}/idsell/{idsell}", produces = "application/json")
 	public ResponseEntity<System_User> registerSell(@PathVariable Long iduser,
 			                                        @PathVariable Long idsell){
-		
 		/*Here would be the sell process*/
 		//System_User saveduser = userRepository.save(system_user);
 		
 		return new ResponseEntity("Id user: " + iduser + " Id sell: " + idsell, HttpStatus.OK);
-		
 	}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<System_User> update(@RequestBody System_User system_user){
+		
+		/*Other routines before update*/
+		
+		System_User saveduser = userRepository.save(system_user);
+		
+		return new ResponseEntity<System_User>(saveduser, HttpStatus.OK);
+		
+	}	
+
+	
 	
 }
 
