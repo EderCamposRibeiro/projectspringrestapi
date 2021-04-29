5dc48695222dc74cd3e704fbe216d2afdde3ff01
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-04-29 08:42:57 -0300

    API Control errors

---
+++ src/main/java/project/api/rest/ObjectErrors.java
@@ -0,0 +1,22 @@
+package project.api.rest;
+
+public class ObjectErrors {
+	
+	private String error;
+	
+	private String code;
+	
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
