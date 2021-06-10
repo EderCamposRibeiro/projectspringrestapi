9d8cf98535af890266d16f91f21f675520689d9c
Eder Campos Ribeiro <edercribeiro@gmail.com>
2021-06-10 17:57:21 -0300

    Creating pagination

--- src/main/java/project/api/rest/controller/IndexController.java
+++ src/main/java/project/api/rest/controller/IndexController.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
@@ -13,6 +12,9 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -57,7 +59,7 @@ public class IndexController {
 		return new ResponseEntity<System_User>(user.get(), HttpStatus.OK);
 	}	
 	
-	/* Serviço RESTful */
+	/* RESTful Service */
 	@GetMapping(value = "/{id}", produces = "application/json")
 	@CacheEvict(value = "cacheuser", allEntries = true)
 	@CachePut("cacheuser")
@@ -71,7 +73,7 @@ public class IndexController {
 	/*RESTful service/*
 	/*We are going to suppose that the user charging is a slow process
 	 * and we want to control it with cache to make the process faster*/
-	@GetMapping(value = "/{id}", produces = "application/json", headers = "X-API-Version=v1")
+	/*@GetMapping(value = "/{id}", produces = "application/json", headers = "X-API-Version=v1")
 	@CacheEvict(value = "cacheuserV1", allEntries = true)
 	@CachePut("cacheuserV1")		
 	public ResponseEntity<UserDTO> initV1(@PathVariable(value = "id") Long id){
@@ -80,10 +82,10 @@ public class IndexController {
 		
 		System.out.println("Processing version 1");
 		return new ResponseEntity<UserDTO>(new UserDTO(user.get()), HttpStatus.OK);
-	}
+	}*/
 	
 	/*RESTful service*/
-	@GetMapping(value = "/{id}", produces = "application/json", headers = "X-API-Version=v2")
+	/*@GetMapping(value = "/{id}", produces = "application/json", headers = "X-API-Version=v2")
 	@CacheEvict(value = "cacheuserV2", allEntries = true)
 	@CachePut("cacheuserV2")	
 	public ResponseEntity<System_User> initV2(@PathVariable(value = "id") Long id) {
@@ -91,7 +93,7 @@ public class IndexController {
 		Optional<System_User> user = userRepository.findById(id);
 		System.out.println("Processing version 2");		
 		return new ResponseEntity<System_User>(user.get(), HttpStatus.OK);
-	}
+	}*/
 	
 	@DeleteMapping(value = "/{id}", produces = "application/text")
 	public String delete(@PathVariable("id") Long id) {
@@ -100,29 +102,86 @@ public class IndexController {
 		
 		return "OK";
 	}
+
+	@DeleteMapping(value = "/{id}/sell", produces = "application/text")
+	public String deleteSell(@PathVariable("id") Long id) {
+		
+		userRepository.deleteById(id);
+		
+		return "OK";
+	}	
 	
 	@GetMapping(value = "/", produces = "application/json")
 	@CacheEvict(value = "cacheusers", allEntries = true)
 	@CachePut("cacheusers")
-	public ResponseEntity<List<System_User>> users () throws InterruptedException{
+	public ResponseEntity<Page<System_User>> users () throws InterruptedException{
 		
-		List<System_User> list = (List<System_User>) userRepository.findAll();
+		PageRequest page = PageRequest.of(0, 5, Sort.by("name"));
+		
+		Page<System_User> list = userRepository.findAll(page);
+		
+		//List<System_User> list = (List<System_User>) userRepository.findAll();
 		
 		//Thread.sleep(6000); /*It hold on a process for about 6 seconds to simulate a slow execution*/
 		
-		return new ResponseEntity<List<System_User>>(list, HttpStatus.OK);
+		return new ResponseEntity<Page<System_User>>(list, HttpStatus.OK);
 	}
+	
+	@GetMapping(value = "/page/{page}", produces = "application/json")
+	@CacheEvict(value = "cacheusers", allEntries = true)
+	@CachePut("cacheusers")
+	public ResponseEntity<Page<System_User>> usersPage (@PathVariable("page") int page) throws InterruptedException{
+		
+		PageRequest pagination = PageRequest.of(page, 5, Sort.by("name"));
+		
+		Page<System_User> list = userRepository.findAll(pagination);
+		
+		return new ResponseEntity<Page<System_User>>(list, HttpStatus.OK);
+	}
+	
 
 	/*END-POINT find user by name*/
 	@GetMapping(value = "/userByName/{name}", produces = "application/json")
 	@CacheEvict(value = "cacheusers", allEntries = true)
 	@CachePut("cacheusers")
-	public ResponseEntity<List<System_User>> userByName (@PathVariable("name") String name) throws InterruptedException{
+	public ResponseEntity<Page<System_User>> userByName (@PathVariable("name") String name) throws InterruptedException{
 		
-		List<System_User> list = (List<System_User>) userRepository.findUserByName(name);
+		PageRequest pageRequest = null;
+		Page<System_User> list = null;
 		
-		return new ResponseEntity<List<System_User>>(list, HttpStatus.OK);
+		if (name == null || (name != null && name.trim().isEmpty()) || name.equalsIgnoreCase("undefined")) { /* Name not informed*/
+			pageRequest = PageRequest.of(0, 5, Sort.by("name"));
+			list = userRepository.findAll(pageRequest);
+		} else {
+			pageRequest = PageRequest.of(0, 5, Sort.by("name"));
+			list = userRepository.findUserByNamePage(name, pageRequest);
+		}
+		
+		
+		return new ResponseEntity<Page<System_User>>(list, HttpStatus.OK);
 	}
+
+	/*END-POINT find user by name*/
+	@GetMapping(value = "/userByName/{name}/page/{page}", produces = "application/json")
+	@CacheEvict(value = "cacheusers", allEntries = true)
+	@CachePut("cacheusers")
+	public ResponseEntity<Page<System_User>> userByNamePage (@PathVariable("name") String name, @PathVariable("page") int page) throws InterruptedException{
+		
+		PageRequest pageRequest = null;
+		Page<System_User> list = null;
+		
+		if (name == null || (name != null && name.trim().isEmpty()) || name.equalsIgnoreCase("undefined")) { /* Name not informed*/
+			pageRequest = PageRequest.of(page, 5, Sort.by("name"));
+			list = userRepository.findAll(pageRequest);
+		} else {
+			pageRequest = PageRequest.of(page, 5, Sort.by("name"));
+			list = userRepository.findUserByNamePage(name, pageRequest);
+		}
+		
+		
+		return new ResponseEntity<Page<System_User>>(list, HttpStatus.OK);
+	}
+	
 	
 	@PostMapping(value = "/", produces = "application/json")
 	public ResponseEntity<System_User> register(@RequestBody @Valid System_User system_user) throws Exception{
--- src/main/java/project/api/rest/repository/UserRepository.java
+++ src/main/java/project/api/rest/repository/UserRepository.java
@@ -2,39 +2,62 @@ package project.api.rest.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import project.api.rest.model.System_User;
 
 @Repository
-public interface UserRepository extends CrudRepository<System_User, Long> {
+public interface UserRepository extends JpaRepository<System_User, Long> {
 	
 	@Query("select u from System_User u where u.login = ?1")
 	System_User findUserByLogin(String login);
 	
 	@Query("select u from System_User u where u.name like %?1%")
-	List<System_User> findUserByName(String name);	
+	List<System_User> findUserByName(String name);
+	
+	@Query(value = "select constraint_name from information_schema.constraint_column_usage where table_name = 'users_role' and   column_name = 'role_id' and   constraint_name <> 'unique_role_user';", nativeQuery = true)
+	String queryConstraintRole();	
+	
+	@Transactional
+	@Modifying
+	@Query(value="INSERT INTO users_role (user_id, role_id) VALUES(?1, (select id from role where name_role = 'ROLE_USER'));", nativeQuery=true)
+	void insertAccessRoleStandard(Long idUser);	
 	
 	@Transactional
 	@Modifying
 	@Query(nativeQuery = true, value = "update System_User set token = ?1 where login = ?2")
 	void updateTokeUser(String token, String login );
 	
-	@Query(value = "select constraint_name from information_schema.constraint_column_usage where table_name = 'users_role' and   column_name = 'role_id' and   constraint_name <> 'unique_role_user';", nativeQuery = true)
-	String queryConstraintRole();
+
 	
 	/*@Transactional
 	@Modifying
 	@Query(value="alter table users_role DROP CONSTRAINT ?1;", nativeQuery= true)
 	void removeConstraintRole(String constraint);*/
 	
-	@Transactional
-	@Modifying
-	@Query(value="INSERT INTO users_role (user_id, role_id) VALUES(?1, (select ID from role where name_role = 'ROLE_USER'));", nativeQuery=true)
-	void insertAccessRoleStandard(Long idUser);
 
+
+	default Page<System_User> findUserByNamePage(String name, PageRequest pageRequest) {
+		System_User user = new System_User();
+		user.setName(name);
+		
+		ExampleMatcher exampleMatcher = ExampleMatcher.matchingAny()
+				.withMatcher("name", ExampleMatcher.GenericPropertyMatchers
+						.contains().ignoreCase());
+		
+		Example<System_User> example = Example.of(user, exampleMatcher);
+		
+		Page<System_User> returned = findAll(example, pageRequest);
+		
+		return returned;
+	}
+		
 }
